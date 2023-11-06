3c36894e0908420fa63504291dc4488226bfb0a3
Настя <59655848+Anastasia-gif@users.noreply.github.com>
2023-11-07 04:04:40 +0800

    Update index.js
    add commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//позволяет разбирать тела запросов, хранящиеся в req.body, выступая в роли ПО промежуточного слоя для серверов, основанных на Express
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const db = require('./queries');
 
+//app и port переменные
 const app = express();
 const port = 3000;
 
@@ -11,14 +14,17 @@ app.use (bodyParser.urlencoded({
 })
 );
 
+//указываем маршрут для поиска GET запроса по корневому / URL-адресу и вернем некоторый JSON:
 app.get('/',(request, response)=>{
     response.json({Info:'WEBAPI c использованием Node.js, Express, PostgreSQL'});
 });
 
+//Настройте приложения на прослушивание через указанный порт:
 app.listen(port,()=>{
     console.log(`Приложение запущено на порту - ${port}`);
 });
 
+//Каждой конечной точки мы установим метод HTTP-запроса, URL-путь конечной точки и соответствующую функцию:
 app.get('/genre', db.getAllGenres);
 app.post('/genre', db.addGenre);
 app.delete('/genre/:id', db.deleteGenre);
